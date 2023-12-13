aac0de8e3f1b3cd9a9b4f6b9b88224569a21ff88
omer daniely <omer1998120@gmail.com>
2023-12-13 15:39:56 +0200

    sky and block created

--- ex5_PEPSE/pepse/world/Terrain.java
+++ ex5_PEPSE/pepse/world/Terrain.java
@@ -1,4 +1,19 @@
 package pepse.world;
 
+import danogl.collisions.GameObjectCollection;
+import danogl.util.Vector2;
+
 public class Terrain {
+    private final GameObjectCollection gameObjects;
+    private final int groundLayer;
+    private int groundHeightAtX0;
+
+    public Terrain(GameObjectCollection gameObjects,
+                   int groundLayer, Vector2 windowDimensions,
+                   int seed) {
+
+        this.gameObjects = gameObjects;
+        this.groundLayer = groundLayer;
+    }
+
 }
