a945fe929af9dc9dfe15cba7c00c48492d6a02d5
omer daniely <omer1998120@gmail.com>
2023-12-15 11:24:51 +0200

    Night added

--- ex5_PEPSE/PepseGameManager.java
+++ ex5_PEPSE/PepseGameManager.java
@@ -4,12 +4,14 @@ import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import danogl.util.Vector2;
 import pepse.world.Sky;
 import pepse.world.Terrain;
+import pepse.world.daynight.Night;
 
 public class PepseGameManager extends GameManager {
 
-    // todo - choose th right numbers
+    // todo - choose the right numbers
     private static final int MIN_X_TERRAIN = -30;
     private static final int MAX_X_TERRAIN = 1800;
 
@@ -20,11 +22,13 @@ public class PepseGameManager extends GameManager {
                                WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener,
                 windowController);
-        Sky.create(gameObjects(), windowController.getWindowDimensions(),
-                Layer.BACKGROUND);
+        Vector2 windowDimensions = windowController.getWindowDimensions();
+        Sky.create(gameObjects(), windowDimensions, Layer.BACKGROUND);
         Terrain terrain = new Terrain(gameObjects(), Layer.STATIC_OBJECTS,
-                windowController.getWindowDimensions(), 5);
+                windowDimensions, 5);
         terrain.createInRange(MIN_X_TERRAIN, MAX_X_TERRAIN);
+        Night.create(gameObjects(), windowDimensions, 26,
+                Layer.FOREGROUND);
     }
 
     public static void main(String[] args) {
