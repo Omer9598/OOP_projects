d54685d47a6f0e849ea52f561276e80448544658
omer daniely <omer1998120@gmail.com>
2024-01-05 10:50:50 +0200

    some fixes for submission

--- ex1_ChatterBot/Chat.java
+++ ex1_ChatterBot/Chat.java
@@ -2,27 +2,21 @@ import java.util.Scanner;
 
 public class Chat {
     public static void main(String[] args) {
-
         // Illegal replies arrays
         String[] bot1IllegalReplies = {"what ", "say I should say "};
         String[] bot2IllegalReplies = {"whaaat ", "say say "};
-
         // Legal replies arrays
         String[] bot1LegalReplies = {"You want me to say <phrase>, do you?" +
                 " alright: <phrase>"};
         String[] bot2LegalReplies = {"say <phrase>? okay: <phrase>"};
-
         // Creating an array of bots
         ChatterBot[] botsArr = {new ChatterBot("Sammy", bot1IllegalReplies,
                 bot1LegalReplies),
                 new ChatterBot("Ruthy", bot2IllegalReplies,
                         bot2LegalReplies)};
-
         // Create the first statement for the bots
         String statement =  "Omer";
-
         Scanner scanner = new Scanner(System.in);
-
         // Starting the infinite loop
         while (true) {
             for (ChatterBot bot : botsArr) {
@@ -34,4 +28,3 @@ public class Chat {
         }
     }
 }
-
